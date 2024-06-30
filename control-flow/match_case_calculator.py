6182b9dee8ed394b339d864c4c4f9fff36e2374b
Imranovic <119802137+Imranovic@users.noreply.github.com>
2024-06-30 20:58:57 +0000

    modified

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,11 +2,18 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 match operation:
-    case "+": print ("The result is", num1 + num2,".")
-    case "-": print ("The result is", num1 - num2,".")
-    case "*": print ("The result is", num1 * num2,".")
+    case "+": 
+        result =  num1 + num2
+        print (f"The result is {result}.")
+    case "-": 
+        result = num1 - num2
+        print (f"The result is {result}.")
+    case "*": 
+        result = num1 * num2
+        print (f"The result is {result}.")
     case "/": 
         if num2 == 0:
             print ("Cannot divide by zero.")
         else:
-            print ("The result is",num1/num2,".")
+            result = num1/num2
+            print (f"The result is {result}.")
