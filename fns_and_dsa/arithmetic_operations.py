baeabff68d1a499e0f79b2a16764fff0cb99628c
Imranovic <119802137+Imranovic@users.noreply.github.com>
2024-07-05 08:52:09 +0000

    added arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add": 
+            result =  num1 + num2
+            print (f"The result is {result}.")
+        case "subtract": 
+            result = num1 - num2
+            print (f"The result is {result}.")
+        case "multiply": 
+            result = num1 * num2
+            print (f"The result is {result}.")
+        case "divide": 
+            if num2 == 0:
+                print ("Cannot divide by zero.")
+            else:
+                result = num1/num2
+                print (f"The result is {result}.")
+
+# perform_operation(2,3, "subtract")
