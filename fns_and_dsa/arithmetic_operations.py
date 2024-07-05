79d3cfd7045fa93acf4cd551884029b81bb19348
Imranovic <119802137+Imranovic@users.noreply.github.com>
2024-07-05 09:00:46 +0000

    updated using if else statements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,19 +1,39 @@
-def perform_operation(num1, num2, operation):
-    match operation:
-        case "add": 
-            result =  num1 + num2
-            print (f"The result is {result}.")
-        case "subtract": 
-            result = num1 - num2
-            print (f"The result is {result}.")
-        case "multiply": 
-            result = num1 * num2
-            print (f"The result is {result}.")
-        case "divide": 
-            if num2 == 0:
-                print ("Cannot divide by zero.")
-            else:
-                result = num1/num2
-                print (f"The result is {result}.")
+# def perform_operation(num1, num2, operation):
+#     match operation:
+#         case "add": 
+#             result =  num1 + num2
+#             print (f"The result is {result}.")
+#         case "subtract": 
+#             result = num1 - num2
+#             print (f"The result is {result}.")
+#         case "multiply": 
+#             result = num1 * num2
+#             print (f"The result is {result}.")
+#         case "divide": 
+#             if num2 == 0:
+#                 print ("Cannot divide by zero.")
+#             elif:
+#                 result = num1/num2
+#                 print (f"The result is {result}.")
+#     return result
 
-# perform_operation(2,3, "subtract")
+
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return  num1 + num2
+        # print (f"The result is {result}.")
+    elif operation == "subtract": 
+        return num1 - num2
+        # print (f"The result is {result}.")
+    elif operation == "multiply":
+        return num1 * num2
+        # print (f"The result is {result}.")
+    elif operation == "divide": 
+        if num2 == 0:
+            print ("Cannot divide by zero.")
+        else:
+            return num1/num2
+            # print (f"The result is {result}.")
+
+
+print(perform_operation(2,3, "subtract"))
