32d76d1234448caeaf5e47a5aacbe230927d2933
Imranovic <119802137+Imranovic@users.noreply.github.com>
2024-07-21 22:50:02 +0000

    added book class

---
+++ oop/book_class.py
@@ -0,0 +1,12 @@
+class Book:
+    def _init_(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def _del_(self):
+        print (f"Deleting {self.title}")
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+    def _repr_(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
