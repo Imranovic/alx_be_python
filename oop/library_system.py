43d834ba5f517699fcce475d447f20a959ecefbb
Imranovic <119802137+Imranovic@users.noreply.github.com>
2024-07-21 23:47:45 +0000

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,13 +1,59 @@
+# class Book:
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
+
+#     def __str__(self):
+#         return f"Book: {self.title} by {self.author}"
+
+#     def __repr__(self):
+#         return f"Book('{self.title}', '{self.author}')"
+
+# class EBook(Book):
+#     def __init__(self, title, author, file_size):
+#         super().__init__(title, author)
+#         self.file_size = file_size
+
+#     def __str__(self):
+#         return f"EBook: {self.title} by {self.author} [EBook, {self.file_size}MB]"
+
+#     def __repr__(self):
+#         return f"EBook('{self.title}', '{self.author}', {self.file_size})"
+    
+
+# class PrintBook(Book):
+#     def __init__(self, title, author, page_count):
+#         super().__init__(title, author)
+#         self.page_count = page_count
+
+#     def __str__(self):
+#         return f"PrintBook: {self.title} by {self.author} [PrintBook, {self.page_count} pages]"
+
+#     def __repr__(self):
+#         return f"PrintBook('{self.title}', '{self.author}', {self.page_count})"
+    
+# class Library:
+#     def __init__(self):
+#         self.books = []
+
+#     def add_book(self, book):
+#         self.books.append(book)
+
+#     def list_books(self):
+#         for book in self.books:
+#             print(book)
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
     def __str__(self):
-        return f"Book:{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}')"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
@@ -15,11 +61,11 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook:{self.title} by {self.author} [EBook, {self.file_size}MB]"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
     def __repr__(self):
         return f"EBook('{self.title}', '{self.author}', {self.file_size})"
-    
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -27,11 +73,12 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook:{self.title} by {self.author} [PrintBook, {self.page_count} pages]"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
     def __repr__(self):
         return f"PrintBook('{self.title}', '{self.author}', {self.page_count})"
-    
+
+
 class Library:
     def __init__(self):
         self.books = []
